d7bbc5ff5588ed9584191e74cedfe4c34d9ba496
Florence-wangui <florencenyokabiwangui@gmail.com>
2023-06-19 08:34:34 +0300

    Green Kiosk

--- projects/greenkiosk/greenkiosk/settings.py
+++ projects/greenkiosk/greenkiosk/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inventory',
+    'customer',
+    'orders',
+    'product_cart',
+    'payment',
+    'delivery',
+    'reviews_and_rating',
+    'user_login',
 ]
 
 MIDDLEWARE = [
